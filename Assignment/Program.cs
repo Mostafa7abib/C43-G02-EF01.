5a1ed1d345ebb49558c2d5db4a06434b19945783
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-02-03 16:28:20 +0200

    Q1 - Get the first 3 orders from customers in Washington

---
+++ Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using static Assignment.ListGenerators;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 - Get the first 3 orders from customers in Washington
+            var Result = CustomerList.Where(C=> C.City == "Washington").Take(3);
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+        }
+    }
+}
